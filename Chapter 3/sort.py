3f2b1c7d568b106428e288134089688336cdae2f
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-20 18:33:14 -0400

    Add files via upload

---
+++ Chapter 3/sort.py
@@ -0,0 +1,17 @@
+pieces = [ 'butts', 'boobs', 'ass', 'titties' ]
+pieces.sort()
+print(pieces)
+pieces.sort(reverse=True)
+print(pieces)
+pieces = [ 'butts', 'boobs', 'ass', 'titties' ]
+print('Here is the original list!')
+print(pieces)
+print('Here is the sorted list!')
+print(sorted(pieces))
+print('Here is the original list again!')
+print(pieces)
+pieces.reverse()
+print(pieces)
+pieces.reverse()
+print(pieces)
+len(pieces)
