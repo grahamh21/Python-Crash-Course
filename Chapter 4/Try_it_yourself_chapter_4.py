e6c12218314f20bee65ec94eb1ab228b001addd2
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-24 17:09:36 -0400

    Add files via upload

--- Chapter 4/Try_it_yourself_chapter_4.py
+++ Chapter 4/Try_it_yourself_chapter_4.py
@@ -5,5 +5,33 @@ for pizza in pizzas:
 	print('I love ' + pizza.title() + ' pizza!')
 print('Man boobs!\n')
 
-#Try it yourself 4-2
-print('Try it Yourself: 4-2\n')
+#Try it yourself 4-3
+print('Try it Yourself: 4-3\n')
+
+for squa in range(1,21):
+	print(squa)
+print('\n')
+
+#Try it yourself 4-4
+print('Try it Yourself: 4-4\n')
+
+#million = list(range(1,1000001))
+#print(million)
+
+#Try it yourself 4-5
+print('Try it Yourself: 4-5\n')
+million = list(range(1,1000001))
+print(min(million))
+print(max(million))
+print(sum(million))
+
+#Try it yourself 4-6
+print('Try it Yourself: 4-6\n')
+
+for blast in range(1,21,2):
+	print(blast)
+
+blasty = list(range(1,21,2))
+print(blasty)
+
+
---
+++ Chapter 4/Try_it_yourself_chapter_4_2.py
@@ -0,0 +1,6 @@
+#Try_it_yourself 4-7
+print('Try it yourself 4-7')
+threes = list(range(3,31,3))
+for three in threes:
+	print(three)
+
