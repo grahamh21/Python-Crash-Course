fc80c72976c53e087ee74b35f0da9625fb92692f
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-24 16:22:50 -0400

    Add files via upload

--- Chapter 4/loops.py
+++ Chapter 4/loops.py
@@ -1,1 +1,15 @@
+suckers = ['carol', 'anthony', 'greg']
+for sucker in suckers:
+	print(sucker.upper() + ' is a bad muhfuh!')
+#every indented line following the 'for loop' is considered inside the loop
+	print('Beeooottch ' + sucker.upper() + '\n')
+#added the print( string '\n') to give it more space
+
+print('Thank you to all the Jabberwockies!\n')
+
+dogs = ['lily', 'otis', 'tae', 'murphy']
+for dog in dogs:
+	print(dog)
+
+
 a
