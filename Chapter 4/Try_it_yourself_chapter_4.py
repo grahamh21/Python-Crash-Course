33ef794bf10485ecdec988786c020cc49b66bccb
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-24 16:32:51 -0400

    Add files via upload

--- Chapter 4/Try_it_yourself_chapter_4.py
+++ Chapter 4/Try_it_yourself_chapter_4.py
@@ -1,1 +1,9 @@
-a
+#Try it yourself 4-1
+print('Try it Yourself: 4-1\n')
+pizzas = ['pepperoni', 'cheese', 'veggie', 'meat']
+for pizza in pizzas:
+	print('I love ' + pizza.title() + ' pizza!')
+print('Man boobs!\n')
+
+#Try it yourself 4-2
+print('Try it Yourself: 4-2\n')
