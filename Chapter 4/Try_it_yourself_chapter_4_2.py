753b55067e16fe2dc4f1e7c2a39354dec68fc282
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-25 16:14:47 -0400

    Add files via upload

--- Chapter 4/Try_it_yourself_chapter_4_2.py
+++ Chapter 4/Try_it_yourself_chapter_4_2.py
@@ -20,3 +20,29 @@ print(cubed)
 
 #range() function generates a series of numbers
 #from there, you can manipulate it in a list comprehension or other ways
+
+#Try_it_yourself 4-10
+print('Try it yourself 4-10: Slices')
+
+colors = ['red', 'blue', 'orange', 'green', 'yellow', 'black', 'white']
+print('The first three items in the list are:')
+print(colors[0:3])
+print('The middle three items in the list are:')
+print(colors[2:5])
+print('The last three items in the list are:')
+print(colors[-3:])
+
+#Try_it_yourself 4-11
+print('Try it yourself 4-11: pizzas')
+my_pizzas = ['cheese', 'pepperoni', 'mushroom', 'meat', 'veggie', 'cream']
+friend_pizza = my_pizzas[:]
+print(my_pizzas)
+print(friend_pizza)
+my_pizzas.append('frank')
+friend_pizza.append('charlie')
+print('My favorite pizzas are:')
+for pizzas in my_pizzas:
+	print(pizzas)
+print('\n')
+for pizzas2 in friend_pizza:
+	print(pizzas2)
--- Chapter 4/slices.py
+++ Chapter 4/slices.py
@@ -1,4 +1,4 @@
-pens = ['gl', 'cross', 'bic', 'highlighter', 'quarks']
+pens = ['gl', 'cross', 'bic', 'highlighter', 'quarks', 'adams', 'clints', 'splintz', 'bonez']
 print(pens[0:2])
 #similar to range() function, have to increase by one for the number ie
 # range(1,11) specifies 1 through 10
@@ -12,3 +12,22 @@ print(pens[3:])
 
 #this also works for negative numbers as well
 print(pens[0:-3])
+
+print('Here are my favorite pen types:')
+for pen in pens[:4]:
+	print(pen.title())
+#using slice to loop only a portion of the items on the list
+
+cars = ['dodge', 'buick', 'ram', 'chevy', 'bmw']
+trucks = cars[:]
+
+print('My favorite cars are:')
+print(cars)
+
+print('Trucks are also:')
+print(trucks)
+
+cars.append('copter')
+trucks.append('flash gordon')
+print(cars)
+print(trucks)
