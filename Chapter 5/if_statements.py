4e0d77c0f4628d93d1dd97e9af2b820153360754
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-06-27 12:36:06 -0400

    Add files via upload

---
+++ Chapter 5/if_statements.py
@@ -0,0 +1,13 @@
+ladies = ['grace', 'colleen', 'lisa', 'carla', 'kristin', 'krista']
+for lady in ladies:
+    if lady == 'lisa':
+        print(lady.upper())
+    else:
+        print(lady.title())
+
+
+#boobs = ['huge']
+#if boobs == 'small'
+#    print('False!')
+#else print('true')
+
