03fe1e3467ff5519149d7f24e01c109c7756474d
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-07-10 08:33:40 -0400

    Add files via upload

---
+++ Chapter 6/modify_dictionaries-2.py
@@ -0,0 +1,6 @@
+alien_2 = {'name' : 'bob', 'city' : 'rochester', 'age' : 23 , 'state': 'Ohio' }
+print(alien_2)
+print('\n')
+
+del alien_2['age']
+print(alien_2)
--- Chapter 6/modify_dictionaries.py
+++ Chapter 6/modify_dictionaries.py
@@ -9,3 +9,20 @@ alien_0 = {'x_position' : 0 , 'y_position' : 25 , 'speed' : 'slow' }
 print(alien_0)
 print('\n')
 
+print('Original x-position: ' + str(alien_0['x_position']))
+print('Original y-position: ' + str(alien_0['y_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+#adjusting the x_position of the alient
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print('\n')
+print('The new x-position of the alien is: ' + str(alien_0['x_position']))  
+
+alien_2 = {'name' : 'bob', 'city' : 'rochester', 'age' : 23 , 'state': 'Ohio' }
+print(alien_2)
