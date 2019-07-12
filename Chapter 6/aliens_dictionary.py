0a4541ddb08e6d34581f7989094a615ef1ab4bd7
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-07-12 14:19:10 -0400

    Add files via upload

--- Chapter 6/aliens_dictionary.py
+++ Chapter 6/aliens_dictionary.py
@@ -1,8 +1,24 @@
 aliens = []
 
-for alien_number in range(30):
+for alien_number in range(31):
     new_alien = {'color': 'green', 'points' : 5, 'speed' : 'slow'}
     aliens.append(new_alien)
+    
 for alien in aliens[:5]:
     print(alien)
 print('...')
+print('The total number of aliens is equal to: ' + str(alien_number))     
+print('\n')
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['points'] = 15
+        alien['speed'] = 'fast'
+
+for alien in aliens[:5]:
+    print(alien)
+        
--- Chapter 6/nesting_dictionaries.py
+++ Chapter 6/nesting_dictionaries.py
@@ -22,11 +22,3 @@ print('\n')
 for heyward1 in howardz:
     print(heyward1.values())
 
-aliens = []
-
-for alien_number in range(30):
-    new_alien = {'color': 'green', 'points' : 5, 'speed' : 'slow'}
-    aliens.append(new_alien)
-for alien in aliens[:5]:
-    print(alien)
-print('...')
