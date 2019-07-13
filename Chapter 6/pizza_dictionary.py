ba6e385b0b300f8c787e540593371fad710186d4
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-07-13 15:50:37 -0400

    Add files via upload

---
+++ Chapter 6/pizza_dictionary.py
@@ -0,0 +1,26 @@
+pizza = {'crust' : 'thick', 'toppings' : ['cheese', 'pepperoni', 'mushrooms', 
+'peppers']}
+
+#summarizing the order
+print('You ordered a ' + pizza['crust'] + 'crust pizza with the following ' +
+ 'toppings!')
+for topping in pizza['toppings']:
+    print(topping)
+##############################################################################
+    
+favorite_toppings = { 'graham' : ['cheese', '],
+    'leo' : ['spinach', 'mushrooms', 'feta'],
+    'meghan' : ['sand', 'gravy', 'water'],
+    'andrew' : ['leaves', 'lead', 'magnets'],
+    }
+for name, toppings in favorite_toppings.items():
+    if len(toppings) > 1:
+#by including the len() command, we refined the program to adjust grammar 
+#to say favorite topping is instead of favorite toppings are
+        print('\nThe favorite pizza toppings of ' + 
+        str(name.title()) + ' are:')
+    elif len(toppings) <= 1:
+        print('\nThe favorite pizza topping of ' + 
+        str(name.title()) + ' is:')
+    for topping in toppings:
+            print('\t' + topping)
