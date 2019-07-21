5ed4aad1da6c5f75ead82dddef32d2a58f0a4fc4
grahamh21 <52039219+grahamh21@users.noreply.github.com>
2019-07-21 12:37:01 -0400

    Add files via upload

---
+++ Chapter 8/try_it_yourself_8-1.py
@@ -0,0 +1,11 @@
+#Try it yourself 8-1:
+def display_message():
+    '''Tell's something about what I have learned in this chaper'''
+    print('A function tells python to execute the code in the function')
+display_message()
+
+#Try it yourself 8-2:
+def favorite_book(title):
+    '''Function that works with parameters and arguments'''
+    print('One of my favorite books is ' + title.title() + '.')
+favorite_book('KITCHEN CONFIDENTIAL')
